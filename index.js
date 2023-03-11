897f045efe13cbff02f654b199e63b2601536120
suereis <n01598930@humber.ca>
2023-03-10 21:30:40 -0500

    lab7

---
+++ index.js
@@ -0,0 +1,24 @@
+function firstFuction() {
+    document.getElementById("christmas").style.display = "block";
+}
+
+function secondFuction() {
+    document.getElementById("newYear").style.display = "block";
+}
+
+function thirdFuction() {
+    document.getElementById("easter").style.display = "block";
+}
+
+
+function functionHidden1() {
+    document.getElementById("christmas").style.display = "none";
+}
+
+function functionHidden2() {
+    document.getElementById("newYear").style.display = "none";
+}
+
+function functionHidden3() {
+    document.getElementById("easter").style.display = "none";
+}
